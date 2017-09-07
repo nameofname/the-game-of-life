70e6ce988db6e7c6ee2d17782badde3142699871
nameofname <nameofemail@gmail.com>
2017-09-07 01:16:28 -0400

    more code styling and trying to fix bugs

--- public/js/life.js
+++ public/js/life.js
@@ -13,6 +13,7 @@
 // create a function that lets the user click the squares to generate the first matrix. Collect the matrix from the table and feed it to new_mx, then feed that result to change_styles
 var game_started = false;
 var moused = false;
+var internval;
 
 $(document).ready(function() {
 
@@ -24,18 +25,16 @@ $(document).ready(function() {
         moused = false;
     });
 
-    $('table').on('mouseenter', 'td', function() {
-        if (!game_started && moused)
-        {
+    $('table#GameTable').on('mouseenter', 'td', function() {
+        if (!game_started && moused) {
             $(this).toggleClass('on');
         }
     });
 
     // Controls: start, pause, clear, follow:
     $('#start').click(function() {
-        $(this).attr('disabled',true);
-        if (!game_started)
-        {
+        $(this).attr('disabled', true);
+        if (!game_started) {
             start_game();
         }
         game_started = true;
@@ -53,17 +52,17 @@ $(document).ready(function() {
     });
 
     // Pre-loaded styles on click event
-    $('#pulsar').click(function() { preload_styles(pulsar); });
-    $('#gun').click(function() { preload_styles(gun); });
+    $('#Pulsar').click(function() { preload_styles(pulsar); });
+    $('#Gun').click(function() { preload_styles(gun); });
 
     // Change the number of cells.
     $('.size_change').click(function() {
         new_table();
     });
 
+    new_table();
 });
 
-var namespaced_time;
 
 // start the game.
 function start_game() {
@@ -85,14 +84,14 @@ function start_game() {
 
 // stop (pause) the game
 function stop_game() {
-    clearInterval(namespaced_time);
+    clearInterval(internval);
 }
 
 // start the interval based on user input- time.
 function start_int(mx) {
     var numnum = parseInt($('#speed').val());
     var the_mx = mx;
-    namespaced_time = setInterval(function() {
+    internval = setInterval(function() {
         the_mx = new_mx(the_mx);
         change_styles(the_mx);
     }, numnum);
@@ -102,14 +101,15 @@ function start_int(mx) {
 
 // create a function to take a matrix and assign styles to the table
 function change_styles(mx) {
-    $('td').removeClass('on');
-    for (y in mx)
-    {
-        for (x in mx[y])
-        {
-            if (mx[y][x] == true)
-            {
-                $('#' + y).find('.'+x).addClass('on');
+
+    for (var y in mx) {
+        for (var x in mx[y]) {
+            var cell = $('.row-' + y).find('.col-' + x);
+
+            if (mx[y][x] == true) {
+                cell.addClass('on');
+            } else {
+                cell.removeClass('on');
             }
         }
     }
@@ -119,70 +119,51 @@ function change_styles(mx) {
 function preload_styles(arr) {
     var w = arr[0].length -1;
     var h = arr.length -1;
-    console.log(w);
     var tw = $('table tr#0 td').length;
-    console.log(tw);
     var new_arr = [];
-    // first calculate the width of the table relative to the wdth of the array,
-    // then the width of one side (minus 1 if the diff is odd)
-    if (tw >= w && tw >= h)
-    {
-        load_up();
-    }
-    else
-    {
-        alert('the table size you have chosen is a bit too small for this pattern');
-    }
-    function load_up()
-    {
+
+    function load_up() {
+
         var diffX = tw - w;
         var diffY = tw - h;
         var side;
         var top_side;
-        if (diffX % 2 != 0)
-        {
+        var incrY = 0;
+
+        if (diffX % 2 != 0) {
             diffX -= 1;
         }
-        if (diffY % 2 != 0)
-        {
+
+        if (diffY % 2 != 0) {
             diffY -= 1;
         }
         side = diffX / 2;
         top_side = diffY / 2;
-        console.log('side: ' + side);
-        console.log(top_side);
-        calc = top_side + w;
-        console.log('a calc: ' +  calc);
         // then add the top and sides, and add the contents of the array in the middle
-        var incrY = 0;
-        for (var y=0; y<tw; y++)
-        {
+
+        for (var y = 0; y < tw; y++) {
             new_arr[y] = [];
-            console.log('this is y: ' + y);
-            if (top_side < y && y < (top_side + w))
-            {
+            if (top_side < y && y < (top_side + w)) {
                 incrY ++;
             }
             var incrX = 0;
-            for (x=0; x<tw; x++)
-            {
+
+            for (var x = 0; x < tw; x++) {
                 // if you are in the top or bottom margins...
-                if (y < top_side && y > (top_side + w))
-                {
+
+                if (y < top_side && y > (top_side + w)) {
                     new_arr[y][x] = false;
-                }
                 //else if (top_side < y > (top_side + w))
                 // else you are in the middle on the y axis
-                else
-                {
+
+                } else {
                     // if you are in the x- margin
-                    if (side < x && x < (side + w))
-                    {
+
+                    if (side < x && x < (side + w)) {
                         new_arr[y][x] = arr[incrY][incrX];
                         incrX ++;
-                    }
-                    else
-                    {
+
+                    } else {
                         //console.log('icrY: ' + incrY);
                         new_arr[y][x] = false;
                     }
@@ -190,6 +171,15 @@ function preload_styles(arr) {
             }
         }
     }
+
+    // first calculate the width of the table relative to the width of the array,
+    // then the width of one side (minus 1 if the diff is odd)
+    if (tw >= w && tw >= h) {
+        load_up();
+    } else {
+        alert('the table size you have chosen is a bit too small for this pattern');
+    }
+
     change_styles(new_arr);
 }
 
@@ -232,38 +222,30 @@ function get_neighbors(mx, _x, _y)
     var x = parseInt(_x);
     var y = parseInt(_y);
     var n = 0;
-    for (y1 = y-1; y1 <= y+1; y1++)
-    {
-        for (x1 = x-1; x1 <= x+1; x1++)
-        {
+
+    for (var y1 = y - 1; y1 <= y + 1; y1++) {
+        for (var x1 = x - 1; x1 <= x + 1; x1++) {
             var y2 = in_range(y1, mx.length-1);
             var x2 = in_range(x1, mx[0].length-1);
-            if (mx[y2][x2] == true && not_equiv(x2,y2))
-            {
+            if (mx[y2][x2] == true && not_equiv(x2,y2)) {
                 n ++;
             }
         }
     }
-    function in_range(num, len)
-    {
-        if (num == -1)
-        {
+
+    function in_range(num, len) {
+        if (num == -1) {
             num = len;
-        }
-        else if (num > len)
-        {
+        } else if (num > len) {
             num = 0;
         }
         return num;
     }
-    function not_equiv(x2,y2)
-    {
-        if (x2 == x && y2 == y)
-        {
+
+    function not_equiv(x2,y2) {
+        if (x2 == x && y2 == y) {
             return false;
-        }
-        else
-        {
+        } else {
             return true;
         }
     }
@@ -272,17 +254,17 @@ function get_neighbors(mx, _x, _y)
 
 // create a table the number of cells specified.
 function new_table() {
-    $('table').empty();
-    var y = parseInt($('input#cell_num').val());
-    //var table = $('<table>');
-    for (i=0; i<y; i++)
-    {
-        var new_tr = $('<tr>').attr({'class' : 'y' , 'id' : i});
-        for (x=0; x<y; x++)
-        {
-            new_tr.append($('<td>').attr('class',x));
+    var y = parseInt($('input#cell_num').val()) || 20;
+
+    $('table#GameTable').empty();
+
+    for (var i = 0; i < y; i++) {
+        var new_tr = $('<tr>').attr('class', 'row-' + i);
+
+        for (var x=0; x<y; x++) {
+            new_tr.append($('<td>').attr('class', 'col-' + x));
         }
-        $('table').append(new_tr);
+        $('table#GameTable').append(new_tr);
     }
 }
 
